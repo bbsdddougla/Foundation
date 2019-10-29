896f37f5002696a5aa66f1fd83ac8a9d42f96aab
Drew Douglas <dddouglas@brillianceweb.com>
2019-10-29 14:09:33 -0400

    Request object for getting content

--- src/Foundation/Features/FoundationCal/GetContentByPublishDateRequest.cs
+++ src/Foundation/Features/FoundationCal/GetContentByPublishDateRequest.cs
@@ -1,6 +1,17 @@
-﻿namespace Foundation.Features.FoundationCal
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foundation.Features.FoundationCal
 {
     public class GetContentByPublishDateRequest
     {
+        public DateTime BeginDate {get;set;}
+
+        public DateTime EndDate {get;set;}
+
+        public IEnumerable<Type> ContentTypes {get;set;}
+
+        public string FetchPublished {get;set;}
+
     }
 }
