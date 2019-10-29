86ff98c475984208d507bce85e61d275339c37b6
Drew Douglas <dddouglas@brillianceweb.com>
2019-10-29 14:08:04 -0400

    Foundation calendar service skeleton

---
+++ src/Foundation/Features/FoundationCal/GetContentByPublishDateRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Foundation.Features.FoundationCal
+{
+    public class GetContentByPublishDateRequest
+    {
+    }
+}
---
+++ src/Foundation/Features/FoundationCal/IContentCalendarService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using EPiServer.Core;
+
+namespace Foundation.Features.FoundationCal
+{
+    public interface IContentCalendarService
+    {
+        IEnumerable<IContent> GetContentByPublishDate(GetContentByPublishDateRequest request);
+    }
+}
