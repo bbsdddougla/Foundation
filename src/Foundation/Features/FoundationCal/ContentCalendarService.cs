1ee7149e7ae3a8115383c3c8e085ce125cd96d0b
Drew Douglas <dddouglas@brillianceweb.com>
2019-10-29 14:11:39 -0400

    Add not implemented service for getting content

---
+++ src/Foundation/Features/FoundationCal/ContentCalendarService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+
+namespace Foundation.Features.FoundationCal
+{
+    [ServiceConfiguration(typeof(IContentCalendarService))]
+    public class ContentCalendarService  : IContentCalendarService
+    {
+        public IEnumerable<IContent> GetContentByPublishDate(GetContentByPublishDateRequest request) => throw new System.NotImplementedException();
+    }
+}
